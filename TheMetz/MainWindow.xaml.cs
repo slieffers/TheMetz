efa67202c9574fba5de93b29a505697850b26f7d
slieffers <slieffers@cfse.biz>
2025-02-11 13:41:48 -0500

    Add initial solution and application for TheMetz project
    Includes .gitignore, a WPF application using .NET 8.0, and ADO pull request data processing services. Also adds project configuration files for Visual Studio and JetBrains Rider IDEs.

---
+++ TheMetz/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using TheMetz.Services;
+
+namespace TheMetz;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private AdoDataService _dataService;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        const string orgUrl = "https://tfs.clarkinc.biz/DefaultCollection";
+
+        const string personalAccessToken = "j3dcue4ijcpz6qxmdzb6uvdm6t6bgxaw2d3tcn6ymswbvuv7y7ra";
+            
+        var connection =
+            new VssConnection(new Uri(orgUrl), new VssBasicCredential(string.Empty, personalAccessToken));
+        _dataService = new AdoDataService(connection);
+        
+        ResultsList.Items.Add("Devs with PR comments in last 2 weeks");
+    }
+
+    private async Task LoadData()
+    {
+        ResultsList.Items.Clear();
+
+        ResultsList.Items.Add("Loading...");
+
+        IEnumerable<KeyValuePair<string, int>> test = await _dataService.ShowCommentCounts();
+
+        ResultsList.Items.Clear();
+
+        foreach (KeyValuePair<string, int> keyValuePair in test)
+        {
+            ResultsList.Items.Add($"{keyValuePair.Key}: {keyValuePair.Value}");
+        }
+
+    }
+
+    private void ResultsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (ResultsList.SelectedItem == null)
+        {
+            return;
+        }
+        
+        PRResults.Document.Blocks.Clear();
+            
+        var selectedAuthor = ResultsList.SelectedItem.ToString();
+            
+        var authorParagraph = new Paragraph(new Run(selectedAuthor));
+        PRResults.Document.Blocks.Add(authorParagraph);
+
+        string authorName = selectedAuthor![..selectedAuthor.IndexOf(':')];
+        List<(string Title, string Url)> commentLinks = _dataService.GetDeveloperCommentLinks(authorName);
+
+        foreach ((string Title, string Url) linkInfo in commentLinks)
+        {
+            var paragraph = new Paragraph();
+            var hyperlink = new Hyperlink(new Run(linkInfo.Title))
+            {
+                NavigateUri = new Uri(linkInfo.Url),
+                Cursor = Cursors.Hand
+            };
+
+            hyperlink.PreviewMouseLeftButtonDown += (s, args) =>
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = linkInfo.Url,
+                    UseShellExecute = true
+                });
+            };
+
+            paragraph.Inlines.Add(hyperlink);
+            PRResults.Document.Blocks.Add(paragraph);
+        }
+        ResultsList.SelectedItem = null;
+    }
+    
+    private async void FetchButtonClicked(object sender, RoutedEventArgs e)
+    {
+        await LoadData();
+    }
+}
---
+++ TheMetz/Services/AdoDataService.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
+using System.Collections.Concurrent;
+
+namespace TheMetz.Services
+{
+    internal class AdoDataService
+    {
+        private readonly VssConnection _connection;
+        private readonly List<GitPullRequest> _pullRequests = [];
+        private readonly List<string> _projectNames = ["Marketplace", "Concorde", "Atlas", "Specialty Channel Development"];
+        private static readonly ConcurrentDictionary<string, List<(string Title, string Url)>> DeveloperCommentLinks = new();
+
+        public AdoDataService(VssConnection connection)
+        {
+            _connection = connection;
+        }
+
+        private async Task LoadPullRequests()
+        {
+            using var gitClient = await _connection.GetClientAsync<GitHttpClient>();
+
+            DateTime fromDate = DateTime.UtcNow.AddDays(-14);
+
+            foreach (string projectName in _projectNames)
+            {
+                _pullRequests.AddRange(await gitClient.GetPullRequestsByProjectAsync(
+                    project: projectName,
+                    searchCriteria: new GitPullRequestSearchCriteria { MinTime = fromDate  }
+                ));
+            }
+        }
+        
+        public async Task<IEnumerable<KeyValuePair<string, int>>> ShowCommentCounts()
+        {
+            (List<string> CustomerOptimizationTeamMembers, List<string> OtherTeamMembers) allTeamMembers = GetAllTeamMembersAsync();
+            
+            await LoadPullRequests();
+            
+            using var gitClient = await _connection.GetClientAsync<GitHttpClient>();
+
+            var developerCommentCount = new ConcurrentDictionary<string, int>();
+
+            await Task.WhenAll(_pullRequests.Select(async pr =>
+            {
+                List<GitPullRequestCommentThread>? threads = await gitClient.GetThreadsAsync(
+                    repositoryId: pr.Repository.Id,
+                    pullRequestId: pr.PullRequestId
+                );
+                
+                IEnumerable<string> authorDisplayNames = threads.SelectMany(t => t.Comments)
+                    .Where(c => c.CommentType != CommentType.System)
+                    .GroupBy(c => c.Author.DisplayName)
+                    .Select(c => c.Key);
+                foreach (string? authorDisplayName in authorDisplayNames)
+                {
+                    if (authorDisplayName == pr.CreatedBy.DisplayName
+                        || !allTeamMembers.CustomerOptimizationTeamMembers.Contains(authorDisplayName))
+                    {
+                        continue;
+                    }
+                    
+                    if (DeveloperCommentLinks.TryGetValue(authorDisplayName, out List<(string Title, string Url)>? value) 
+                        && value.Any(r => r.Title == pr.Title))
+                    {
+                        continue;
+                    }
+                        
+                    developerCommentCount.AddOrUpdate(
+                        authorDisplayName,
+                        1,
+                        (_, count) => count + 1
+                    );
+                    DeveloperCommentLinks.AddOrUpdate(
+                        authorDisplayName,
+                        _ => new List<(string, string)> { (pr.Title, GetFormattedPrUrl(pr)) },
+                        (_, existingList) => { existingList.Add((pr.Title, GetFormattedPrUrl(pr))); return existingList; }
+                    );
+                }
+            }));
+
+            return developerCommentCount;
+        }
+
+        private static string GetFormattedPrUrl(GitPullRequest pr)
+        {
+            return $"https://tfs.clarkinc.biz/DefaultCollection/{pr.Repository.ProjectReference.Name}/_git/{pr.Repository.Name}/pullrequest/{pr.PullRequestId}";
+        }
+
+        private static (List<string> CustomerOptimizationTeamMembers, List<string> OtherTeamMembers) GetAllTeamMembersAsync()
+        {
+            var customerOptimizationTeamMembers = new List<string>
+            {
+                "Dylan Manning",
+                "David Acker",
+                "Kerry Hannigan",
+                "Liudmila Solovyeva",
+                "Andrew Chanthavisith",
+                "Michal Lesniewski",
+                "Khrystyna Kregenbild"
+            };
+            var otherTeamMembers = new List<string>
+            {
+                "Phil Gathany",
+                "Brandon George",
+                "Shane Lieffers",
+                "Shawn Dreier"
+            };
+            
+            return (customerOptimizationTeamMembers, otherTeamMembers);
+        }
+
+        public List<(string Title, string Url)> GetDeveloperCommentLinks(string developerName)
+        {
+            return DeveloperCommentLinks[developerName].ToList();
+        }
+    }
+}
